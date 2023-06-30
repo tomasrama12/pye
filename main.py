c01d281977d182355366789a6e4cc2e8d932dbae
tomasrama12 <tomasramaviana@gmail.com>
2023-06-30 02:46:48 -0300

    d

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 
-def contar_valores_true(csv_file, columna, valor):
+def contar_valores_desempleo(csv_file):
     conteoDesempleo = 0
 
     with open(csv_file, 'r') as archivo:
@@ -8,10 +8,30 @@ def contar_valores_true(csv_file, columna, valor):
 
         for fila in lector_csv:
             #print(fila)
-            if fila[columna] == valor:
+            if fila['Desempleo'] == '1':
                 conteoDesempleo += 1
 
     return conteoDesempleo
+
+def contar_valores_pea(csv_file):
+    conteoPea = 0
+
+    with open(csv_file, 'r') as archivo:
+        lector_csv = csv.DictReader(archivo, delimiter=';')
+
+        for fila in lector_csv:
+            #print(fila)
+            if fila['PEA'] == '1':
+                conteoPea += 1
+
+    return conteoPea
+
+def TDesempleo(csv_file):
+    return contar_valores_desempleo(csv_file) / contar_valores_pea(csv_file)
+
+
+
+
 
 def contar_total_filas(csv_file):
     with open(csv_file, 'r') as archivo:
@@ -22,15 +42,16 @@ def contar_total_filas(csv_file):
     return total_filas
 
 def porcentajeDesmpelados(csv_file):
-    return contar_valores_true(csv_file, 'Desempleo', '1') / contar_total_filas(csv_file)
+    return contar_valores_desempleo(csv_file) / contar_total_filas(csv_file)
 
 # Ejemplo de uso
 archivo_csv = 'datos.csv'
 columna_objetivo = 'Desempleo'
 valor_objetivo = '1'
 
-cantidad_true = contar_valores_true(archivo_csv, columna_objetivo, valor_objetivo)
-print(f"La cantidad de valores '{valor_objetivo}' en la columna '{columna_objetivo}' es: {cantidad_true}")
+
 print(f"El total del conteo es {contar_total_filas(archivo_csv)}")
 
 print(f"El porcentaje de desempleados es: {porcentajeDesmpelados(archivo_csv)}")
+
+print(f"El porcentaje de Tdesempleados es: {TDesempleo(archivo_csv)}")
